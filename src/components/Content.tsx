d7cec38c8b6e882c5bced445d8c250cfd8c202d4
Augusto Terra <ahterra@MacBook-Air-de-Augusto.local>
2021-03-15 10:42:44 -0300

    breaking app into components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,19 @@
-export function Content() {
-  // Complete aqui
+import React from 'react';
+import { MovieProps } from '../App';
+import {MovieCard} from './MovieCard';
+
+interface ContentProps{
+  movies: MovieProps[];
+}
+
+export function Content({ movies }: ContentProps) {
+  return(
+    <main>
+      <div className="movies-list">
+        {movies.map(movie => (
+          <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { GenreResponseProps } from '../App';
+
+interface HeaderProps{
+    selectedGenre: GenreResponseProps;
+}
+
+const Header = ({ selectedGenre }: HeaderProps) => {
+    return(
+        <header>
+            <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+        </header>
+    );
+}
+
+export default Header;
